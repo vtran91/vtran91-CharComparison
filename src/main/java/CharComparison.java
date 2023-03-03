450502ac54b3b7ec83971b5d4799d530762a5fbc
vtran91 <vtran16@bellsouth.net>
2023-03-03 03:26:49 +0000

    Completed CharCompare Coding Challenge

--- src/main/java/CharComparison.java
+++ src/main/java/CharComparison.java
@@ -11,6 +11,18 @@ public class CharComparison {
      * @return -1 if A is less than B, 1 if A is greater than B, and 0 if the two arrays are identical.
      */
     public int compare(char[] a, char[] b){
-        return 0;
+        String aStr = "";
+        String bStr = "";
+        for (int i = 0; i < a.length; i++)
+        {
+            aStr += a[i];
+        }
+
+        for (int j = 0; j < b.length; j++)
+        {
+            bStr += b[j];
+        }
+
+        return aStr.compareTo(bStr) > 0? 1: aStr.compareTo(bStr); 
     }
 }
